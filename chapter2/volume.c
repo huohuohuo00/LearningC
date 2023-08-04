d2a1863ae98aa693adc7c66f4a98efeb9e2721cf
SaeruHikari <3045349520@qq.com>
2023-08-04 22:26:30 +0800

    一些改进

--- chapter2/volume.c
+++ chapter2/volume.c
@@ -5,10 +5,12 @@ int main()
     printf("Please enter the number of cups:____\b\b\b\b");
     float cup;
     scanf("%f", &cup);
-    float pint = cup / 2;
-    int   ounce = cup * 8;
-    float tablespoon = ounce / 2;
-    float teaspoon = tablespoon / 3;
+    const float pint = cup / 2;
+    const int   ounce = cup * 8;
+    // const float tablespoon = ounce / 2;
+    // Result of integer division used in a floating point context, possible loss of precision
+    const float tablespoon = (float)ounce / 2;
+    const float teaspoon = tablespoon / 3;
     printf("\n%f cups equals %f pints.\n", cup, pint);
     printf("%f cups equals %d ounces.\n", cup, ounce);
     printf("%f cups equals %f tablespoons.\n", cup, tablespoon);
